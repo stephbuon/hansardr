78edae14c4547298377f6e6c97159a2502908b26
stephbuon <steph.buon@gmail.com>
2022-01-16 09:54:01 -0600

    debug

---
+++ R/hansard_1830.R
@@ -0,0 +1,28 @@
+#' The 19th-century British Parliamentary debates for the decade 1830
+#'
+#' Hansard corpus data for the decade 1830. To also access information about speaker, date, and debate title, join this data frame with debate_metadata on
+#' sentence_id. The variables are as follows:
+#'
+#' @docType data
+#'
+#' @usage hansard_1830
+#'
+#' @format A data frame with 957327 rows and 2 variables:
+#'
+#' sentence_id
+#' A unique ID assigned to each sentence of the Hansard corpus.
+#'
+#' text
+#' A sentence spoken by a member of parliament.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(hansard_1830)
+#'
+#' @references Buongiorno, Steph; Kalescky, Robert; Godat, Eric; Cerpa, Omar Alexander; Guldi, Jo (2021) 
+#' (\href{https://doi.org/10.7910/DVN/ZCYJH8})
+#'
+#' @source \href{https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/ZCYJH8}{Harvard Dataverse}
+#'
+"hansard_1830"
