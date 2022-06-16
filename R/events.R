82a0bd597136f2fffa0f8bde45ea916ca54dca89
stephbuon <steph.buon@gmail.com>
2022-06-16 15:04:17 -0500

    added events

---
+++ R/events.R
@@ -0,0 +1,33 @@
+#' The 19th-century British Parliamentary debates metadata for the decade 1840
+#'
+#' The Hansard corpus debate metadata retains information about the debates such as the debate name and the speaker.
+#' The variables are as follows:
+#'
+#' @docType data
+#'
+#' @usage events
+#'
+#' @format A data frame with 48485 rows and 4 variables:
+#'
+#' event_name
+#' The name of the event
+#'
+#' scholar_assigned_date
+#' The date assigned to the event by a historian.
+#'
+#' period
+#' The time period in which the event was mentioned.
+#'
+#' occurances
+#' The number of times the event was stated in a period.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(events)
+#'
+#' @references Buongiorno, Steph; Kalescky, Robert; Godat, Eric; Cerpa, Omar Alexander; Guldi, Jo (2021)
+#' (\href{https://doi.org/10.7910/DVN/ZCYJH8})
+#'
+"events"
+
