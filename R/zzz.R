d5c149d0258aae1f564ff8361c4c848b0d921d01
stephbuon <steph.buon@gmail.com>
2022-09-30 09:47:05 -0500

    test

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+#library(curl)
+
+.onLoad <- function(libname, pkgname) {
+
+  # hansardr_data <- "~/projects/"
+  # if(file.exists(paste0(hansardr_data, "data/"))) { } else {
+  #   url <- "https://smu.box.com/shared/static/34sxstotmquq2ud81r449nomlm0fcplp"
+  #   fname <- paste0(hansardr_data, "data.zip")
+  #   #curl_download(url, fname)
+  #   download.file(url, fname, mode = "wb")
+  #   unzip(paste0(hansardr_data, "data.zip"), exdir = paste0(hansardr_data, "data")) }
+
+  }
