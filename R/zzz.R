98df5c5a93ac7613f097c9bbc064f373dcc414ae
stephbuon <steph.buon@gmail.com>
2022-09-30 12:34:58 -0500

    added onload

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,13 @@
-#library(curl)
-
 .onLoad <- function(libname, pkgname) {
 
-  # hansardr_data <- "~/projects/"
-  # if(file.exists(paste0(hansardr_data, "data/"))) { } else {
-  #   url <- "https://smu.box.com/shared/static/34sxstotmquq2ud81r449nomlm0fcplp"
-  #   fname <- paste0(hansardr_data, "data.zip")
-  #   #curl_download(url, fname)
-  #   download.file(url, fname, mode = "wb")
-  #   unzip(paste0(hansardr_data, "data.zip"), exdir = paste0(hansardr_data, "data")) }
+  hansardr_path <- paste0(.libPaths(), "/hansardr/")[1]
+  if(file.exists(paste0(hansardr_path, "data/"))) { invisible()
+    } else {
+      url <- "https://smu.box.com/shared/static/34sxstotmquq2ud81r449nomlm0fcplp"
+      fname <- paste0(hansardr_path, "data.zip")
+      download.file(url, fname, mode = "wb")
 
+      zip_file <- paste0(hansardr_path, "data.zip")
+      unzip(zip_file, exdir = paste0(hansardr_path, "data"))
+      file.remove(zip_file, "data.zip")) }
   }
