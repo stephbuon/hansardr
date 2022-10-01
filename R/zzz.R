62093c3379b86562de9962a9e70030e9342bcc7b
stephbuon <steph.buon@gmail.com>
2022-09-30 22:24:19 -0500

    fixing href problem

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,10 @@
+quiet <- function(x) {
+  sink(tempfile())
+  on.exit(sink())
+  invisible(force(x))
+}
+
+
 .onLoad <- function(libname, pkgname) {
 
   hansardr_path <- paste0(.libPaths(), "/hansardr/")[1]
@@ -5,9 +12,19 @@
     } else {
       url <- "https://smu.box.com/shared/static/34sxstotmquq2ud81r449nomlm0fcplp"
       fname <- paste0(hansardr_path, "data.zip")
+      print("Downloading the c19 Hansard Corpus.")
       download.file(url, fname, mode = "wb")
 
       zip_file <- paste0(hansardr_path, "data.zip")
-      unzip(zip_file, exdir = paste0(hansardr_path, "data"))
-      file.remove(zip_file, "data.zip") }
+      unzip(zip_file, exdir = paste0(hansardr_path, "uncompressed_data"))
+
+      from <- paste0(hansardr_path, "uncompressed_data/data/")
+      to <- paste0(hansardr_path, "data/")
+
+      quiet(file.move(from, to))
+      quiet(file.remove(paste0(hansardr_path, "data.zip")))
+      quiet(file.remove(paste0(hansardr_path, "uncompressed_data")))
+
+      }
+
   }
