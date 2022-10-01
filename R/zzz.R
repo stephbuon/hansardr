17af49b779982b8b20f54f60ef145bf53acd0c7a
stephbuon <steph.buon@gmail.com>
2022-09-30 21:21:32 -0500

    fixed onload typo

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,13 @@
 .onLoad <- function(libname, pkgname) {
 
-  hansardr_path <- paste0(.libPaths(), "/hansardr/")[1]
-  if(file.exists(paste0(hansardr_path, "data/"))) { invisible()
-    } else {
-      url <- "https://smu.box.com/shared/static/34sxstotmquq2ud81r449nomlm0fcplp"
-      fname <- paste0(hansardr_path, "data.zip")
-      download.file(url, fname, mode = "wb")
-
-      zip_file <- paste0(hansardr_path, "data.zip")
-      unzip(zip_file, exdir = paste0(hansardr_path, "data"))
-      file.remove(zip_file, "data.zip") }
+  # hansardr_path <- paste0(.libPaths(), "/hansardr/")[1]
+  # if(file.exists(paste0(hansardr_path, "data/"))) { invisible()
+  #   } else {
+  #     url <- "https://smu.box.com/shared/static/34sxstotmquq2ud81r449nomlm0fcplp"
+  #     fname <- paste0(hansardr_path, "data.zip")
+  #     download.file(url, fname, mode = "wb")
+  #
+  #     zip_file <- paste0(hansardr_path, "data.zip")
+  #     unzip(zip_file, exdir = paste0(hansardr_path, "data"))
+  #     file.remove(zip_file, "data.zip") }
   }
