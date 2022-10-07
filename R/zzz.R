65e6005811a412193ee23f501f2bf1a8ff9531d8
stephbuon <steph.buon@gmail.com>
2022-10-07 16:37:59 -0500

    updated DESCRIPTION

--- R/zzz.R
+++ R/zzz.R
@@ -11,18 +11,24 @@
     } else {
       url <- "https://smu.box.com/shared/static/34sxstotmquq2ud81r449nomlm0fcplp"
       fname <- paste0(hansardr_path, "data.zip")
-      print("Downloading the c19 Hansard Corpus.")
-      download.file(url, fname, mode = "wb")
+      packageStartupMessage("Downloading the c19 Hansard Corpus.")
 
-      zip_file <- paste0(hansardr_path, "data.zip")
-      unzip(zip_file, exdir = paste0(hansardr_path, "uncompressed_data"))
+      tryCatch({
+        utils::download.file(url, fname, mode = "wb")
 
-      from <- paste0(hansardr_path, "uncompressed_data/data/")
-      to <- paste0(hansardr_path, "data/")
+        zip_file <- paste0(hansardr_path, "data.zip")
+        utils::unzip(zip_file, exdir = paste0(hansardr_path, "uncompressed_data"))
 
-      quiet(file.move(from, to))
-      quiet(file.remove(paste0(hansardr_path, "data.zip")))
-      quiet(file.remove(paste0(hansardr_path, "uncompressed_data")))
+        from <- paste0(hansardr_path, "uncompressed_data/data/")
+        to <- paste0(hansardr_path, "data/")
+
+        quiet(ff::file.move(from, to))
+        quiet(file.remove(paste0(hansardr_path, "data.zip")))
+        quiet(file.remove(paste0(hansardr_path, "uncompressed_data"))) },
+
+        warning = function(cond) {
+          packageStartupMessage("The c19 Hansard data cannot be downloaded. Please open a GitHub Issue if this problem persists.")
+          packageStartupMessage("https://github.com/stephbuon/hansardr/issues") } )
 
       }
 
