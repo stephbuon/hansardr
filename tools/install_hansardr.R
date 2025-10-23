f681678e53e66d6ad904a271dd8dcf2214816f88
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2025-10-23 12:34:44 -0500

    Add files via upload

---
+++ tools/install_hansardr.R
@@ -0,0 +1,17 @@
+# 1. Ensure remotes is installed
+if (!"remotes" %in% rownames(installed.packages())) {
+  install.packages("remotes")
+}
+
+# 2. Load remotes
+library(remotes)
+
+# 3. Give slow networks plenty of time (30 mins)
+options(timeout = max(1800, getOption("timeout")))
+Sys.setenv(CURL_TIMEOUT = "1800")
+
+# 4. Install hansardr from GitHub
+remotes::install_github(
+  "stephbuon/hansardr",
+  dependencies = TRUE,
+  upgrade = "never")
